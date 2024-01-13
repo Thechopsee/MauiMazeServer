779fcef598e0d9b676d435f11d8f1a1dc358cce8
Thechopsee <74382107+Thechopsee@users.noreply.github.com>
2024-01-13 23:03:41 +0100

    test update

--- tests/RepositoryTest/test_UserRepoTest.py
+++ tests/RepositoryTest/test_UserRepoTest.py
@@ -1,9 +1,10 @@
 import sys
 import pytest
-sys.path.append('/repositories')
+sys.path.append('./repositories')
 from userRepository import UserRepository
 
 
 def test_login():
     ur=UserRepository()
-    assert -1==ur.trytoLoginDatabase("nevim","nevim")
+    assert -1==ur.trytoLoginDatabase("nevim","nevim")[0]
+
