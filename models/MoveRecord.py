f359321835e5cd6d9f1d33587ebf29ba8ab82d44
Thechopsee <74382107+Thechopsee@users.noreply.github.com>
2024-02-12 22:17:44 +0100

    fixes

--- models/MoveRecord.py
+++ models/MoveRecord.py
@@ -1,10 +1,11 @@
 class MoveRecord:
-    def __init__(self, mr_id,percentage_x, percentage_y, hit_wall,delta):
+    def __init__(self, mr_id,percentage_x, percentage_y, hit_wall,delta,cell):
         self.mrID = mr_id
         self.percentagex = percentage_x
         self.percentagey = percentage_y
         self.hitWall = hit_wall
         self.deltaTinMilisec=int(delta)
+        self.cell=cell
 
 # Example usage:
 # move_record = MoveRecord(mr_id=1, position_x=2, position_y=3, percentage_x=0.5, percentage_y=0.8, hit_wall=True)
