423e0d896b8081eaa9c3ace6b12c489b3a429c31
Thechopsee <74382107+Thechopsee@users.noreply.github.com>
2024-02-12 19:07:57 +0100

    added adapters to repository

--- repositories/VCRepository.py
+++ repositories/VCRepository.py
@@ -2,43 +2,22 @@ import sqlite3
 
 class VCRepository:
 	@staticmethod
-	def count_of_unused(database):
-		connection = sqlite3.connect(database)
-		cursor = connection.cursor()
-
-		res=cursor.execute('Select Count(*) from VerificationCodes WHERE used=0')
-		if(res is None):
-			return -1
-		else:
-			return(int(res.fetchall()[0][0]))
+	def count_of_unused(database,adapter):
+		sql='Select Count(*) from VerificationCodes WHERE used=0'
+		return adapter.getOne(database,sql)
 	@staticmethod
-	def save_verification_code(code,database):
-		connection = sqlite3.connect(database)
-		cursor = connection.cursor()
-
-		cursor.execute('INSERT INTO VerificationCodes (code) VALUES (?)', (code,))
-		
-		connection.commit()
-		inserted_id = cursor.lastrowid
-		connection.close()
+	def save_verification_code(code,database,adapter):
+		sql='INSERT INTO VerificationCodes (code) VALUES (?)', code
+		inserted_id = adapter.saveOne(database,sql,code)
 		return inserted_id
 		
 	@staticmethod
-	def updateCode(code,database):
-		connection = sqlite3.connect(database)
-		cursor = connection.cursor()
-
-		cursor.execute("UPDATE VerificationCodes SET used=1 WHERE code='"+str(code)+"'")
+	def updateCode(code,database,adapter):
+		sql="UPDATE VerificationCodes SET used=1 WHERE code='"+str(code)+"'"
+		adapter.getOne(database,sql)
 		
-		connection.commit()
-		connection.close()
-
 	@staticmethod
-	def isCodeTaken(codee,database):
-		con = sqlite3.connect(database)
-		cur = con.cursor()
-		res = cur.execute("SELECT used FROM VerificationCodes WHERE code='"+codee+"'")
-		if(res is None):
-			return -1
-		else:
-			return(int(res.fetchall()[0][0]))
+	def isCodeTaken(codee,database,adapter):
+		
+		sql="SELECT used FROM VerificationCodes WHERE code='"+codee+"'"
+		return adapter.getOne(database,sql)
--- repositories/recordsRepository.py
+++ repositories/recordsRepository.py
@@ -6,25 +6,20 @@ from MoveRecord import MoveRecord
 
 class RecordRepository:
     @staticmethod
-    def saveMovesToDatabase(moves,database):
-        con = sqlite3.connect(database)
-        cur = con.cursor()
-        cur.executemany("INSERT INTO MoveRecord (percentagex,percentagey,hitwall,deltaTime,grID) VALUES (?,?,?,?,?)",moves)
-        con.commit()
+    def saveMovesToDatabase(moves,database,adapter):
+        sql="INSERT INTO MoveRecord (percentagex,percentagey,hitwall,deltaTime,grID) VALUES (?,?,?,?,?)"
+        adapter.saveMany(database,sql,moves)
     @staticmethod
-    def saveRecordtoDatabase(record,database):
-        con = sqlite3.connect(database)
-        cur = con.cursor()
-        cur.execute("INSERT INTO GameRecord (mazeID,userID,timeInMilliSeconds,hitWallsCount,cellPathString) VALUES (?,?,?,?,?)",(record.get("mazeID"),record.get("userID"),record.get("timeInMilliSeconds"),record.get("hitWallsCount"),record.get("cellPathString")))
-        con.commit()
-        last_id = cur.lastrowid
+    def saveRecordtoDatabase(record,database,adapter):
+        sql="INSERT INTO GameRecord (mazeID,userID,timeInMilliSeconds,hitWallsCount,cellPathString) VALUES (?,?,?,?,?)"
+        data=(record.get("mazeID"),record.get("userID"),record.get("timeInMilliSeconds"),record.get("hitWallsCount"),record.get("cellPathString"))
+
+        last_id = adapter.saveOne(database,sql,data)
         return last_id
     @staticmethod
-    def loadRecordByID(grid,database):
-        con = sqlite3.connect(database)
-        cur = con.cursor()
-        res = cur.execute("SELECT * FROM GameRecord WHERE grID="+str(grid))
-        fetched=res.fetchall()
+    def loadRecordByID(grid,database,adapter):
+        sql="SELECT * FROM GameRecord WHERE grID="+str(grid)
+        fetched=adapter.getMany(database,sql)
         gameRecords=[]
         for x in fetched:
             gr=GameRecord(x[0],x[1],x[2],x[3],x[4],x[5])
@@ -34,11 +29,9 @@ class RecordRepository:
             gameRecords.append(gr)
         return gameRecords
     @staticmethod
-    def loadRecordsbyMaze(mid,database):
-        con = sqlite3.connect(database)
-        cur = con.cursor()
-        res = cur.execute("SELECT * FROM GameRecord WHERE mazeID="+str(mid))
-        fetched=res.fetchall()
+    def loadRecordsbyMaze(mid,database,adapter):
+        sql="SELECT * FROM GameRecord WHERE mazeID="+str(mid)
+        fetched=adapter.getMany(database,sql)
         gameRecords=[]
         for x in fetched:
             gr=GameRecord(x[0],x[1],x[2],x[3],x[4],x[5])
@@ -48,11 +41,9 @@ class RecordRepository:
             gameRecords.append(gr)
         return gameRecords
     @staticmethod
-    def loadRecordsbyUser(uid,database):
-        con = sqlite3.connect(database)
-        cur = con.cursor()
-        res = cur.execute("SELECT * FROM GameRecord WHERE userID="+str(uid))
-        fetched=res.fetchall()
+    def loadRecordsbyUser(uid,database,adapter):
+        sql="SELECT * FROM GameRecord WHERE userID="+str(uid)
+        fetched=adapter.getMany(database,sql)
         gameRecords=[]
         for x in fetched:
             gr=GameRecord(x[0],x[1],x[2],x[3],x[4],x[5])
@@ -63,12 +54,6 @@ class RecordRepository:
         return gameRecords
 
     @staticmethod
-    def getMoves(grid,database):
-        con = sqlite3.connect(database)
-        cur = con.cursor()
-        res = cur.execute("SELECT * FROM MoveRecord WHERE grID="+str(grid))
-        
-        if(res is None):
-            return []
-        else:
-            return res.fetchall()
+    def getMoves(grid,database,adapter):
+        sql="SELECT * FROM MoveRecord WHERE grID="+str(grid)
+        return adapter.getMany(database,sql)
