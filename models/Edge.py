fad7dbc4978763ade0b2f8c676e690aca9f1c6b3
Thechopsee <74382107+Thechopsee@users.noreply.github.com>
2024-02-12 17:45:42 +0100

    Update Edge.py

--- models/Edge.py
+++ models/Edge.py
@@ -4,3 +4,9 @@ class Edge:
     def __init__(self, Cell1: int, Cell2: int):
         self.Cell1: int = Cell1
         self.Cell2: int = Cell2
+
+    def serialize(self):
+        return {
+            'Cell1': self.source,
+            'Cell2': self.target,
+        }
